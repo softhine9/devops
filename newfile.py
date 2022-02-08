96d44594c201ea106b075d5b32041040ea45175d
root <root@ip-172-31-0-24.us-east-2.compute.internal>
2022-02-08 20:03:31 +0000

    branch1 commits

---
+++ newfile.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+#print(datetime.today().year)
+#def age ():
+birthyear = int(input("Enter your birthyear:"))
+currentyear = (datetime.today().year)
+age = currentyear - birthyear 
+print("your age is :" , age)
+  #   age();
